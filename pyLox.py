b5d8c759a209777dac393c498170f3eb5f6210e3
unknown <wedlichn@gmail.com>
2022-06-09 00:57:25 +0800

    Moved interpreter creation outside of run

--- pyLox.py
+++ pyLox.py
@@ -6,43 +6,43 @@ from parser import Parser
 from interpreter import Interpreter
 from errors import ErrorHandler
 
-def run(source: str, errorHandler: ErrorHandler) -> None:
+def run(source: str, interpreter: Interpreter, errorHandler: ErrorHandler) -> None:
     scanner = Scanner(source, errorHandler)
     tokens = scanner.scanTokens()
     errorHandler.reportErrors(source)
     parser = Parser(tokens, errorHandler)
     expr = parser.parse()
     errorHandler.reportErrors(source)
-    interpreter = Interpreter(errorHandler)
     interpreter.interpret(expr)
     errorHandler.reportErrors(source)
 
-def runFile(file: str, errorHandler: ErrorHandler) -> None:
+def runFile(file: str, interpreter: Interpreter, errorHandler: ErrorHandler) -> None:
     with open(file, "r") as f:
         source = f.read()
-        if run(source, errorHandler):
+        if run(source, interpreter, errorHandler):
             if errorHandler.hadError:
                 exit(65)
             elif ErrorHandler.hadRuntimeError:
                 exit(70)
 
-def runRepl(errorHandler: ErrorHandler) -> None:
+def runRepl(interpreter: Interpreter, errorHandler: ErrorHandler) -> None:
     print("PyLox REPL:")
     while True:
         errorHandler.hadError = False
         line = input("> ")
         if line == "" or line == "exit": break
-        run(line, errorHandler)
+        run(interpreter, line, errorHandler)
 
 def pyLox():
     errorHandler = ErrorHandler()
+    interpreter = Interpreter(errorHandler)
     if len(sys.argv) > 2:
         print("Usage: python pyLox.py [script]")
         exit(1)
     elif len(sys.argv) == 2:
-        runFile(sys.argv[1], errorHandler)
+        runFile(sys.argv[1], interpreter, errorHandler)
     else:
-        runRepl(errorHandler)
+        runRepl(interpreter, errorHandler)
 
 def tempMain():
     expr = Binary(Unary(Token(TokenType.MINUS, "-", "", 1), Literal(123)), 
